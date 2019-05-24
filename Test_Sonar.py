d997619f566370e0854a6a2392f452fad7b4232d
BKulzick <bryankulczycki@gmail.com>
2019-05-24 10:48:42 -0400

    updating Sonar Test for magic numbers

--- Test_Sonar.py
+++ Test_Sonar.py
@@ -9,7 +9,8 @@ class TestStringMethods(unittest.TestCase):
         ECHO_PIN = "P8_11"
         TRIGGER_PIN = "P8_12"
         timeDuration = .01
-        expected = timeDuration * speed of sound
+        SPEED_OF_SOUND_CM = 17150
+        expected = timeDuration * SPEED_OF_SOUND_CM
         actualDistance = distanceMeasurement(TRIGGER_PIN,ECHO_PIN)
 
         self.assertEqual(expected, actualDistance)
