5badc1c0222af9c508768fa88a556614fe282e1d
BKulzick <bryankulczycki@gmail.com>
2019-05-24 10:47:16 -0400

    updating Sonar Test

--- Test_Sonar.py
+++ Test_Sonar.py
@@ -1,17 +1,18 @@
 import unittest
-from unittest.mock import MagicMock
+from mock import Mock, patch
 from Sonar import distanceMeasurement
 
 class TestStringMethods(unittest.TestCase):
-  
-    @mock.patch('Adafruit_BBIO.output')
+
     @mock.patch('Adafruit_BBIO.input')
     def test_distanceMeasurement(self):
         ECHO_PIN = "P8_11"
         TRIGGER_PIN = "P8_12"
-        distanceMeasurement(TRIGGER_PIN,ECHO_PIN)
+        timeDuration = .01
+        expected = timeDuration * speed of sound
+        actualDistance = distanceMeasurement(TRIGGER_PIN,ECHO_PIN)
 
-        self.assertEqual('foo'.upper(), 'FOO')
+        self.assertEqual(expected, actualDistance)
 
 if __name__ == '__main__':
     unittest.main()
